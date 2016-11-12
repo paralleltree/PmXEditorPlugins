e7294243355a71932ee948500381b0c02e36f7d7
Paralleltree <paralleltree@outlook.com>
2016-11-12 21:33:58 +0900

    Add ContinuousSelection project

---
+++ ContinuousSelection/ContinuousSelection/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ContinuousSelection")]
+[assembly: AssemblyDescription("A selection plugin for Pmx and Pmd Editor")]
+[assembly: AssemblyCompany("paltee.net")]
+[assembly: AssemblyCopyright("Copyright (C) 2016 Paralleltree")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("cc140cbd-264f-4480-a108-3d7b7ea85ac2")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
