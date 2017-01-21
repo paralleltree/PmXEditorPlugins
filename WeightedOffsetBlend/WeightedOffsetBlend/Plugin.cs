860e7155fae83ad3804bcd824d68b20414504d8c
Paralleltree <paralleltree@outlook.com>
2017-01-21 22:40:42 +0900

    Implement a plugin

--- WeightedOffsetBlend/WeightedOffsetBlend/Plugin.cs
+++ WeightedOffsetBlend/WeightedOffsetBlend/Plugin.cs
@@ -2,9 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using PEPlugin;
+
 namespace WeightedOffsetBlend
 {
-    public class Plugin
+    public class Plugin : PEPluginClass
     {
+        private static MainForm FormInstance { get; set; }
+
+        public override string Name
+        {
+            get { return "重み付きオフセット付加"; }
+        }
+
+        public override IPEPluginOption Option
+        {
+            get
+            {
+                return new PEPluginOption(bootup: true);
+            }
+        }
+
+        public override void Run(IPERunArgs args)
+        {
+            if (args.IsBootup)
+            {
+                FormInstance = new MainForm(args.Host);
+                FormInstance.Visible = false;
+                FormInstance.Show();
+                return;
+            }
+
+            FormInstance.Visible = true;
+            FormInstance.BringToFront();
+        }
     }
 }
